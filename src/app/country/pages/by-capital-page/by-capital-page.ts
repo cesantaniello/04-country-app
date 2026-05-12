a00b72dcfab1bfe914ea02567c100e1ffdd17795
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-05-12 13:47:47 +0200

    Routing and folder structure

---
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital-page',
+  imports: [],
+  template: `<p>by-capital-page works!</p>`,
+})
+export class ByCapitalPage {}
