add286cc8e59e82165d62f18c66ab3f3f48978fc
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-05-13 18:05:01 +0200

    Create mapper

--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SearchInput } from "../../components/search-input/search-input";
 import { CountryList } from "../../components/country-list/country-list";
 import { CountryService } from '../../services/country';
+import { RESTCountry } from '../../interfaces/rest-countries.interfaces';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -11,9 +12,22 @@ import { CountryService } from '../../services/country';
 export class ByCapitalPage {
   countryService = inject(CountryService);
 
+  isLoading = signal(false);
+  isError = signal<string|null>(null);
+  countries = signal<RESTCountry[]>([]);
+
   onSearch(query: string) {
-    this.countryService.searchByCapital(query).subscribe((response) => {
-      console.log(response);
+    if(this.isLoading()) return;
+
+    this.isLoading.set(true);
+    this.isError.set(null);
+
+    this.countryService.searchByCapital(query).
+    subscribe((countries) => {
+      this.isLoading.set(false);
+      this.countries.set(countries);
+
+      console.log(countries);
     });
   }
 }
