aa6264867c97bab82a77288ba8694a0d11133dde
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-05-15 14:20:10 +0200

    Search countries

--- src/app/country/services/country.ts
+++ src/app/country/services/country.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interfaces';
-import { catchError, Observable, throwError } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { CountryMapper } from '../mappers/country.mappers';
+import { Country } from '../interfaces/country.interface';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -11,10 +13,11 @@ const API_URL = 'https://restcountries.com/v3.1';
 export class CountryService {
   private http = inject(HttpClient);
 
-  searchByCapital(query: string): Observable<RESTCountry[]> {
+  searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
 
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
+      map((response) => CountryMapper.mapRestCountryArrayToCountryArray(response)),
       catchError((error) => {
         console.error('Error fetching countries by capital:', error);
         return throwError(() => new Error('Capital no encontrada'));
@@ -24,7 +27,14 @@ export class CountryService {
 
   searchByCountry(query: string) {
     query = query.toLowerCase();
-    return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`);
+
+    return this.http.get<RESTCountry[]>(url).pipe(
+      map((response) => CountryMapper.mapRestCountryArrayToCountryArray(response)),
+      catchError((error) => {
+        console.error('Error fetching countries by capital:', error);
+        return throwError(() => new Error('Capital no encontrada'));
+      })
+    );
   }
 
   searchByRegion(region: string) {
