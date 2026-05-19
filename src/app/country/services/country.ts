fcd59a3ea3d7c47217a0ba623e3aece0640f101c
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-05-19 11:11:11 +0200

    Countries cache

--- src/app/country/services/country.ts
+++ src/app/country/services/country.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interfaces';
-import { catchError, Observable, of, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -10,9 +11,10 @@ const API_URL = 'https://restcountries.com/v3.1';
 })
 export class CountryService {
   private http = inject(HttpClient);
-  private readonly queryCacheCapital = new Map<string, RESTCountry[]>();
+  private queryCacheCapital = new Map<string, Country[]>();
+  private queryCacheCountry = new Map<string, Country[]>();
 
-  searchByCapital(query: string): Observable<RESTCountry[]> {
+  searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
 
     const cached = this.queryCacheCapital.get(query);
@@ -21,6 +23,7 @@ export class CountryService {
     }
 
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
+      map((data) => data.map(this.mapToCountry)),
       tap((data) => this.queryCacheCapital.set(query, data)),
       catchError((error) => {
         console.error('Error fetching countries by capital:', error);
@@ -29,10 +32,28 @@ export class CountryService {
     );
   }
 
-  searchByCountry(query: string): Observable<RESTCountry[]> {
+  private mapToCountry(country: RESTCountry): Country {
+    return {
+      cca2: country.cca2,
+      flag: country.flag,
+      flagSvg: country.flags.svg,
+      name: country.name.common,
+      capital: country.capital?.[0] ?? '',
+      population: country.population,
+    };
+  }
+
+  searchByCountry(query: string): Observable<Country[]> {
     query = query.toLowerCase();
 
+    const cached = this.queryCacheCountry.get(query);
+    if (cached !== undefined) {
+      return of(cached);
+    }
+
     return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`).pipe(
+      map((data) => data.map(this.mapToCountry)),
+      tap((data) => this.queryCacheCountry.set(query, data)),
       catchError((error) => {
         console.error('Error fetching countries by name:', error);
         return throwError(() => new Error('País no encontrado'));
